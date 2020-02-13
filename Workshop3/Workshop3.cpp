68417de685cce60aa931c16431ea8677092ca446
Milos Kojic <m.kojic@live.com>
2020-02-13 18:35:57 +0000

    Make item

--- Workshop3/Workshop3.cpp
+++ Workshop3/Workshop3.cpp
@@ -1,6 +1,3 @@
-// Workshop3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -35,7 +32,9 @@ Recipe* recipes;
 Item* getItem(string input) {
 	for (int i = 0; i < totalRecipes; i++)
 	{
-
+		if (items[i].name == input) {
+			return &items[i];
+		}
 	}
 	return nullptr;
 }
@@ -50,12 +49,23 @@ Ingredient* getIngredient(string name) {
 	return nullptr;
 }
 
-void MakeItem(Item* item) {
-	//	Item newItem = new Item(item->name, item->amount++, item->ingredients);
+void makeItem(Item* item) {
+	if (item->recipe->ingredients[0]->amount > 0 && item->recipe->ingredients[1]->amount > 0) {
+		item->recipe->ingredients[0]->amount -= 1;
+		item->recipe->ingredients[1]->amount -= 1;
+		cout << "Item \"" << item->name << "\" created!" << endl;
+	}
+	else {
+		cout << "No enough ingredients to make this item!" << endl;
+	}
 }
 
 void processInput(string input) {
-	if (input == "items") {
+	Item* inputItem = getItem(input);
+	if (inputItem != nullptr) {
+		makeItem(inputItem);
+	}
+	else if (input == "items") {
 		cout << endl << "ITEMS" << endl;
 		cout << "****************" << endl;
 		for (int i = 0; i < totalRecipes; i++)
@@ -87,9 +97,6 @@ void processInput(string input) {
 			cout << ingredients[i].name << " " << ingredients[i].amount << endl;
 		}
 		cout << "****************" << endl;
-	}
-	else if (Item* selectedItem = getItem(input)) {
-		MakeItem(selectedItem);
 	}
 	else if (input == "quit") {
 		exit(1);
@@ -204,14 +211,3 @@ int main()
 		}
 	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
