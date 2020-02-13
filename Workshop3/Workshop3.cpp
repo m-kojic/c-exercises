e8f902832ee15ce680e21c9f1b9fdae360f7bc4f
Milos Kojic <m.kojic@live.com>
2020-02-13 23:53:26 +0000

    Workshop 3 - working with multiple recipes

--- Workshop3/Workshop3.cpp
+++ Workshop3/Workshop3.cpp
@@ -14,15 +14,17 @@ struct Ingredient {
 struct Item {
 	string name;
 	unsigned int amount;
-	Recipe* recipe;
+	Recipe* recipes;
+	int totalRecipes;
 };
 
 struct Recipe {
-	Item* item;
+	string name;
 	Ingredient* ingredients[2];
 };
 
 unsigned int totalRecipes = 0;
+unsigned int totalItems = 0;
 unsigned int totalIngredients = 0;
 
 Item* items;
@@ -30,7 +32,7 @@ Ingredient* ingredients;
 Recipe* recipes;
 
 Item* getItem(string input) {
-	for (int i = 0; i < totalRecipes; i++)
+	for (int i = 0; i < totalItems; i++)
 	{
 		if (items[i].name == input) {
 			return &items[i];
@@ -49,26 +51,50 @@ Ingredient* getIngredient(string name) {
 	return nullptr;
 }
 
-void makeItem(Item* item) {
-	if (item->recipe->ingredients[0]->amount > 0 && item->recipe->ingredients[1]->amount > 0) {
-		item->recipe->ingredients[0]->amount -= 1;
-		item->recipe->ingredients[1]->amount -= 1;
-		cout << "Item \"" << item->name << "\" created!" << endl;
+void makeItem(string name, Recipe* recipe) {
+	if (recipe->ingredients[0]->amount > 0 && recipe->ingredients[1]->amount > 0) {
+		recipe->ingredients[0]->amount -= 1;
+		recipe->ingredients[1]->amount -= 1;
+		cout << "Item \"" << name << "\" created!" << endl;
 	}
 	else {
 		cout << "No enough ingredients to make this item!" << endl;
 	}
 }
 
+void checkForMultipleRecipes(Item* item) {
+	if (item->totalRecipes > 1) {
+		cout << "What recipe do you want to create?" << endl;
+		for (int i = 0; i < item->totalRecipes; i++)
+		{
+			cout << "Option " << i << ": " << item->recipes[i].name << " = "
+				<< item->recipes[i].ingredients[0]->name << " + " << item->recipes[i].ingredients[1]->name << endl;
+		}
+
+		int option;
+		cin >> option;
+		if (option < item->totalRecipes) {
+			Recipe* recipe = &(item->recipes[option]);
+			makeItem(item->name, recipe);
+		}
+		else {
+			cout << "Invalid value!" << endl;
+		}
+	}
+	else {
+		makeItem(item->name, &(item->recipes[0]));
+	}
+}
+
 void processInput(string input) {
 	Item* inputItem = getItem(input);
 	if (inputItem != nullptr) {
-		makeItem(inputItem);
+		checkForMultipleRecipes(inputItem);
 	}
 	else if (input == "items") {
 		cout << endl << "ITEMS" << endl;
 		cout << "****************" << endl;
-		for (int i = 0; i < totalRecipes; i++)
+		for (int i = 0; i < totalItems; i++)
 		{
 			cout << items[i].name << " " << items[i].amount << endl;
 		}
@@ -79,7 +105,7 @@ void processInput(string input) {
 		cout << "****************" << endl;
 		for (int i = 0; i < totalRecipes; i++)
 		{
-			cout << endl << recipes[i].item->name << endl << endl;
+			cout << endl << recipes[i].name << endl << endl;
 			cout << "Ingredients: " << endl;
 			for (unsigned int j = 0; j < 2; j++)
 			{
@@ -115,27 +141,53 @@ void mainLoop() {
 	}
 }
 
+int countItems(string* recipes, unsigned int n)
+{
+	int count = 0;
+	for (int i = 0; i < n; i++)
+	{
+		string name = recipes[i];
+		int j;
+		for ( j = 0; j < i; j++) {
+			string comparedName = recipes[j];
+			if (name == comparedName) {
+				break;
+			}
+		}
+
+		if(i == j)
+			count++;
+	}
+	return count;
+}
+
 void countIngredientsAndRecipes(ifstream& inputFile) {
 	string line;
+	stringstream stringStream;
+	string itemNames[100];
 	while (getline(inputFile, line))
 	{
 		if (line.find("=") != string::npos) {
+			stringStream.str(line);
+			string name;
+			stringStream >> name;
+			itemNames[totalRecipes] = name;
 			totalRecipes++;
 		}
 		else {
 			totalIngredients++;
 		}
-
 	}
+	totalItems = countItems(itemNames, totalRecipes);
+	cout << "totalItems: " << totalItems << endl;
 	cout << "totalRecipes: " << totalRecipes << endl;
 	cout << "totalIngredients: " << totalIngredients << endl;
-
 }
 
 void populateIngredientsAndRecipes(ifstream& inputFile) {
-	items = new Item[totalRecipes];
-	ingredients = new Ingredient[totalIngredients];
 	recipes = new Recipe[totalRecipes];
+	ingredients = new Ingredient[totalIngredients];
+	items = new Item[totalItems];
 
 	string line;
 	unsigned int currentRecipes = 0;
@@ -150,10 +202,15 @@ void populateIngredientsAndRecipes(ifstream& inputFile) {
 		if (line.find("=") != string::npos) {
 			char ignoreChars;
 			Item* item = new Item;
+			item->recipes = new Recipe[totalRecipes];
 			Recipe* recipe = new Recipe;
 			item->amount = 0;
+			item->totalRecipes = 0;
 
-			lineStream >> item->name;
+			string recipeName;
+			lineStream >> recipeName;
+			item->name = recipeName;
+			recipe->name = recipeName;
 			lineStream >> ignoreChars;
 
 			string ingredient1Name;
@@ -168,9 +225,17 @@ void populateIngredientsAndRecipes(ifstream& inputFile) {
 			Ingredient* ingredient2 = getIngredient(ingredient2Name);
 			recipe->ingredients[1] = ingredient2;
 
-			item->recipe = recipe;
-			recipe->item = item;
-			items[currentRecipes] = *item;
+			Item* existingItem = getItem(recipeName);
+			if (existingItem == nullptr) {
+				item->recipes[item->totalRecipes] = *recipe;
+				item->totalRecipes += 1;
+				items[currentRecipes] = *item;
+			}
+			else {
+				existingItem->recipes[existingItem->totalRecipes] = *recipe;
+				existingItem->totalRecipes += 1;
+			}
+
 			recipes[currentRecipes] = *recipe;
 			currentRecipes++;
 		}
